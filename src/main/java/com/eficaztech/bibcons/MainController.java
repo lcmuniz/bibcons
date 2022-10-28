64a9877f5021ad9357b7b4a9c380cb8e01d132eb
Luiz Carlos Muniz <lcmuniz@gmail.com>
2022-10-28 09:59:27 -0300

    inicial

--- src/main/java/com/eficaztech/bibcons/MainController.java
+++ src/main/java/com/eficaztech/bibcons/MainController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-
 @Controller
 public class MainController {
 
